7a6c3a4951c76f657d06f11f00e63fba0dbb3c95
Takashi Kashiwagi <tkashi.al@outlook.com>
2018-11-03 17:45:15 +0900

    IMXRT1060-EVK APL > sb
    StorageBenchMark
    BS = 8192 [BYTE], WP=10.145589 [MB/S], RP=30.319109 [MB/S]
    BS = 16384 [BYTE], WP=20.604749 [MB/S], RP=48.756704 [MB/S]
    BS = 32768 [BYTE], WP=19.104934 [MB/S], RP=57.487784 [MB/S]
    BS = 65536 [BYTE], WP=19.107672 [MB/S], RP=58.021468 [MB/S]
    IMXRT1060-EVK APL >

--- IMXRT1060-EVK_APL/source/common/StorageBenchMark.c
+++ IMXRT1060-EVK_APL/source/common/StorageBenchMark.c
@@ -66,12 +66,19 @@ void CmdStorageBenchMark(uint32_t argc, const char *argv[]){
 	for(uint32_t i=0;i<(sizeof(au32BlockSize)/sizeof(au32BlockSize[0]));i++){
 		uint32_t u32BlockSize = au32BlockSize[i];
 		uint32_t u32LoopCnt = DefTEST_FILE_SIZE / u32BlockSize;
+		uint32_t u32PtnCnt = sizeof(au32Ptn)/sizeof(au32Ptn[0]);
+		double dfpWriteTime = 0;
+		double dfpReadTime = 0;
+		double dfpWriteMBperSec = (double)DefTEST_FILE_SIZE;
+		double dfpReadMBperSec = (double)DefTEST_FILE_SIZE;
+		uint32_t ProgressCnt = 0;
+		char Progress[] = {'\\', '|', '/', '-'};
 
-		mimic_printf("[%s (%d)] u32BlockSize = %lu\r\n", __FUNCTION__, __LINE__, u32BlockSize);	
-		for(uint32_t j=0;j<(sizeof(au32Ptn)/sizeof(au32Ptn[0]));j++){
+		//mimic_printf("[%s (%d)] u32BlockSize = %lu\r\n", __FUNCTION__, __LINE__, u32BlockSize);	
+		for(uint32_t j=0;j<u32PtnCnt;j++){
 			uint32_t u32Ptn = au32Ptn[j];
-	
-			mimic_printf("[%s (%d)] u32Ptn = 0x%08lX\r\n", __FUNCTION__, __LINE__, u32Ptn);	
+				
+			//mimic_printf("[%s (%d)] u32Ptn = 0x%08lX\r\n", __FUNCTION__, __LINE__, u32Ptn);	
 
 			/** Make Test Ptn */
 			for(uint32_t k=0;k<(u32BlockSize/sizeof(uint32_t));k++){
@@ -82,10 +89,17 @@ void CmdStorageBenchMark(uint32_t argc, const char *argv[]){
 			for(uint32_t k=0;k<DefTEST_FILE_NUM;k++){
 				TCHAR szFilePath[256];
 				FRESULT fres;
+				uint32_t StartTick;
+				uint32_t WriteTick;
+				uint32_t ReadTick;
 
-				snprintf(szFilePath, "A:/%03u.dat", k);
-				mimic_printf("[%s (%d)] fopen = <%s>\r\n", __FUNCTION__, __LINE__, szFilePath);
-				fres = f_open(&stFIL, szFilePath, FA_CREATE_ALWAYS);
+				mimic_printf("\r%c", Progress[ProgressCnt]);
+				ProgressCnt++;
+				ProgressCnt %= 4;
+
+				sprintf(szFilePath, "A:/%03lu.dat", k);
+				//mimic_printf("[%s (%d)] fopen = <%s>\r\n", __FUNCTION__, __LINE__, szFilePath);
+				fres = f_open(&stFIL, szFilePath, FA_WRITE | FA_CREATE_ALWAYS);
 				if(FR_OK != fres){
 					mimic_printf("[%s (%d)] f_open NG (%d)\r\n", __FUNCTION__, __LINE__, fres);
 					f_close(&stFIL);
@@ -93,8 +107,8 @@ void CmdStorageBenchMark(uint32_t argc, const char *argv[]){
 				}
 
 
-				mimic_printf("[%s (%d)] f_write\r\n", __FUNCTION__, __LINE__);
-				uint32_t WriteTick = osKernelGetTickCount();
+				//mimic_printf("[%s (%d)] f_write\r\n", __FUNCTION__, __LINE__);
+				StartTick = osKernelGetTickCount();
 
 				for(uint32_t l=0;l<u32LoopCnt;l++){
 					uint32_t bw;
@@ -106,43 +120,60 @@ void CmdStorageBenchMark(uint32_t argc, const char *argv[]){
 						goto _END;
 					}
 				}
-				mimic_printf("[%s (%d)] f_write time = <%lu msec>\r\n", __FUNCTION__, __LINE__, osKernelGetTickCount() - WriteTick);
+				WriteTick = osKernelGetTickCount() - StartTick;
+				//mimic_printf("[%s (%d)] f_write time = <%lu msec>\r\n", __FUNCTION__, __LINE__, WriteTick);
 				f_close(&stFIL);
+				dfpWriteTime += (double)WriteTick;
 
-
-				fres = f_open(&stFIL, szFilePath, FA_OPEN_EXISTING);
+				fres = f_open(&stFIL, szFilePath, FA_READ | FA_OPEN_EXISTING);
 				if(FR_OK != fres){
 					mimic_printf("[%s (%d)] f_open NG (%d)\r\n", __FUNCTION__, __LINE__, fres);
 					f_close(&stFIL);
 					goto _END;
 				}
 
-				mimic_printf("[%s (%d)] f_readd\r\n", __FUNCTION__, __LINE__);
+				//mimic_printf("[%s (%d)] f_read\r\n", __FUNCTION__, __LINE__);
 				
-				uint32_t ReadTick = osKernelGetTickCount();
+				
 
 				for(uint32_t l=0;l<u32LoopCnt;l++){
 					uint32_t br;
 
+					StartTick = osKernelGetTickCount();
 					fres = f_read(&stFIL, pu32ReadBuffer, u32BlockSize, (UINT*)&br);
 					if(FR_OK != fres){
 						mimic_printf("[%s (%d)] f_write NG (%d)\r\n", __FUNCTION__, __LINE__, fres);
 						f_close(&stFIL);
 						goto _END;
 					}
-
+					ReadTick = osKernelGetTickCount() - StartTick;
+					dfpReadTime += (double)ReadTick;
 					if(0!= memcmp(pu32WriteBuffer, pu32ReadBuffer, u32BlockSize))
 					{
 						mimic_printf("memcmp NG\r\n" );
 						goto _END;
 					}
 				}
-				mimic_printf("[%s (%d)] f_read time = <%lu msec>\r\n", __FUNCTION__, __LINE__, osKernelGetTickCount() - ReadTick);
-
 
 				f_close(&stFIL);
+
+				f_unlink(szFilePath);
 			}
 		}
+
+		dfpWriteTime /= (u32PtnCnt * DefTEST_FILE_NUM);
+		dfpReadTime /= (u32PtnCnt * DefTEST_FILE_NUM);
+
+		dfpWriteMBperSec /= dfpWriteTime;
+		dfpWriteMBperSec *= 1000.0;
+		dfpWriteMBperSec /= 1024.0;
+		dfpWriteMBperSec /= 1024.0;
+
+		dfpReadMBperSec /= dfpReadTime;
+		dfpReadMBperSec *= 1000.0;
+		dfpReadMBperSec /= 1024.0;
+		dfpReadMBperSec /= 1024.0;
+		mimic_printf("\rBS = %lu [BYTE], WP=%f [MB/S], RP=%f [MB/S]\r\n", u32BlockSize, dfpWriteMBperSec, dfpReadMBperSec);	
 	}
 
 _END:
