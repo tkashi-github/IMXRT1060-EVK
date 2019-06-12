946d4de13ce60e628715b0c4534e80c6a1ce5c54
Takashi Kashiwagi <tkashi.al@outlook.com>
2019-06-12 23:28:00 +0900

    1060用

--- IMXRT1060-EVK_APL/source/buildlog.h
+++ IMXRT1060-EVK_APL/source/buildlog.h
@@ -1,5 +1,5 @@
 #define DEF_USER_BUILDCOUNT_tkashi 295
-#define DEF_USER_BUILDCOUNT_tkash 1
-#define DEF_PREBUILD_TIME "Mon Jun 10 22:01:04 2019"
-#define DEF_TOTAL_BUILD_COUNT "Build296"
+#define DEF_USER_BUILDCOUNT_tkash 2
+#define DEF_PREBUILD_TIME "Wed Jun 12 00:12:21 2019"
+#define DEF_TOTAL_BUILD_COUNT "Build297"
 
