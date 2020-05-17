1d761a33f859b8a9da206a5188a85777360dec8b
Takashi Kashiwagi <tkashi.al@outlook.com>
2020-05-17 23:04:58 +0900

    MCUxpresso 11.1

--- IMXRT1060-EVK_BOOT/source/buildlog.h
+++ IMXRT1060-EVK_BOOT/source/buildlog.h
@@ -1,4 +1,4 @@
-#define DEF_USER_BUILDCOUNT_tkash 10
-#define DEF_PREBUILD_TIME "Wed Mar 25 21:22:02 2020"
-#define DEF_TOTAL_BUILD_COUNT "Build10"
+#define DEF_USER_BUILDCOUNT_tkash 11
+#define DEF_PREBUILD_TIME "Sun May 17 22:53:09 2020"
+#define DEF_TOTAL_BUILD_COUNT "Build11"
 
